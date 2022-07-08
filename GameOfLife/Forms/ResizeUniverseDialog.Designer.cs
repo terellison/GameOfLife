a2e28f85291c5238b68bcbcf5ddf98993810e484
terellison <ellisonterry4@gmail.com>
2022-07-08 11:34:11 -0400

    changed text align from left to right for height and width removed WaitCursor from height and width text boxes

--- GameOfLife/Forms/ResizeUniverseDialog.Designer.cs
+++ GameOfLife/Forms/ResizeUniverseDialog.Designer.cs
@@ -41,18 +41,20 @@ namespace GameOfLife.Forms
             // widthLabel
             // 
             widthLabel.AutoSize = true;
-            widthLabel.Location = new System.Drawing.Point(280, 147);
+            widthLabel.Location = new System.Drawing.Point(187, 96);
+            widthLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             widthLabel.Name = "widthLabel";
-            widthLabel.Size = new System.Drawing.Size(54, 20);
+            widthLabel.Size = new System.Drawing.Size(38, 13);
             widthLabel.TabIndex = 4;
             widthLabel.Text = "Width:";
             // 
             // okButton
             // 
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(661, 235);
+            this.okButton.Location = new System.Drawing.Point(441, 153);
+            this.okButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(107, 44);
+            this.okButton.Size = new System.Drawing.Size(71, 29);
             this.okButton.TabIndex = 0;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
@@ -60,9 +62,10 @@ namespace GameOfLife.Forms
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(530, 235);
+            this.cancelButton.Location = new System.Drawing.Point(353, 153);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(107, 44);
+            this.cancelButton.Size = new System.Drawing.Size(71, 29);
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -70,35 +73,38 @@ namespace GameOfLife.Forms
             // heightLabel
             // 
             this.heightLabel.AutoSize = true;
-            this.heightLabel.Location = new System.Drawing.Point(280, 100);
+            this.heightLabel.Location = new System.Drawing.Point(187, 65);
+            this.heightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.heightLabel.Name = "heightLabel";
-            this.heightLabel.Size = new System.Drawing.Size(60, 20);
+            this.heightLabel.Size = new System.Drawing.Size(41, 13);
             this.heightLabel.TabIndex = 2;
             this.heightLabel.Text = "Height:";
             // 
             // heightTextBox
             // 
-            this.heightTextBox.Location = new System.Drawing.Point(347, 100);
+            this.heightTextBox.Location = new System.Drawing.Point(231, 65);
+            this.heightTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.heightTextBox.Name = "heightTextBox";
-            this.heightTextBox.Size = new System.Drawing.Size(100, 26);
+            this.heightTextBox.Size = new System.Drawing.Size(68, 20);
             this.heightTextBox.TabIndex = 3;
-            this.heightTextBox.UseWaitCursor = true;
+            this.heightTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // widthTextbox
             // 
-            this.widthTextbox.Location = new System.Drawing.Point(347, 141);
+            this.widthTextbox.Location = new System.Drawing.Point(231, 92);
+            this.widthTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.widthTextbox.Name = "widthTextbox";
-            this.widthTextbox.Size = new System.Drawing.Size(100, 26);
+            this.widthTextbox.Size = new System.Drawing.Size(68, 20);
             this.widthTextbox.TabIndex = 5;
-            this.widthTextbox.UseWaitCursor = true;
+            this.widthTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // ResizeUniverseForm
+            // ResizeUniverseDialog
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(780, 291);
+            this.ClientSize = new System.Drawing.Size(520, 189);
             this.Controls.Add(this.widthTextbox);
             this.Controls.Add(widthLabel);
             this.Controls.Add(this.heightTextBox);
@@ -106,9 +112,10 @@ namespace GameOfLife.Forms
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ResizeUniverseForm";
+            this.Name = "ResizeUniverseDialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Resize Universe";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ResizeUniverseDialog_FormClosed);
